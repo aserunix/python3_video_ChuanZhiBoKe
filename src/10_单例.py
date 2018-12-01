1408fa9c7bc8ca2a6cb7a96ffe4b7f9f709930ca
aser <aserunix@163.com>
2018-12-01 23:17:57 +0800

    Default Changelist

---
+++ src/10_单例.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class MusicPlayer(object):
+    instance = None
+    init_flag=False
+
+    def __new__(cls,*args,**kwargs):
+        if cls.instance is None:
+            cls.instance=super().__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        if MusicPlayer.init_flag:
+            return
+        print("播放器初始化")
+        MusicPlayer.init_flag=True
+
+player1=MusicPlayer()
+print(player1)
+
+player2=MusicPlayer()
+print(player2)
